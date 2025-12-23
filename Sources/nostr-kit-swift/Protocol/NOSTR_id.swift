498396e0c5e8725c985cd06b4d1fb94b46f21dd3
Brock Wyma <brockdwyma@gmail.com>
2025-12-23 11:30:07 -0600

    nostr_id conforms to nostr_tag_value

--- Sources/nostr-kit-swift/Protocol/NOSTR_id.swift
+++ Sources/nostr-kit-swift/Protocol/NOSTR_id.swift
@@ -5,4 +5,4 @@ import RAW_dh25519
 /// The unique id sha256 hash attached to each created `NOSTR_event`.
 /// It can be used for indexing of references for other events.
 /// It is used to prevent accidental duplicate events.
-public struct NOSTR_id:Sendable, Hashable, Comparable, RAW_convertible, RAW_accessible { }
+public struct NOSTR_id:Sendable, Hashable, Comparable, RAW_convertible, RAW_accessible, NOSTR_tag_value { }
